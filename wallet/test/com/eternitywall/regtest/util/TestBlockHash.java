3aa724a79c9f48e346fdf5a70fe76f2dcece3ca3
Riccardo Casatta <riccardo@casatta.it>
2017-11-16 16:11:23 +0100

    printing both testnet and mainnet

--- wallet/test/com/eternitywall/regtest/util/TestBlockHash.java
+++ wallet/test/com/eternitywall/regtest/util/TestBlockHash.java
@@ -2,6 +2,7 @@ package com.eternitywall.regtest.util;
 
 import org.bitcoinj.core.Block;
 import org.bitcoinj.params.VtknNetParams;
+import org.bitcoinj.params.VtknTestNetParams;
 import org.junit.Test;
 
 /**
@@ -12,9 +13,20 @@ public class TestBlockHash {
 
     @Test
     public void testGenesisBlockHash() {
+
+        System.out.println("VtknNetParams");
         final Block genesisBlock = new VtknNetParams().getGenesisBlock();
         System.out.println("genesis hash: " + genesisBlock.getHashAsString());
         System.out.println("merkle root: " + genesisBlock.getMerkleRoot().toString());
+        System.out.println("time: " + genesisBlock.getTime());
+
+        System.out.println();
+        System.out.println("VtknTestNetParams");
+        final Block genesisBlockTestnet = new VtknTestNetParams().getGenesisBlock();
+        System.out.println("genesis hash: " + genesisBlockTestnet.getHashAsString());
+        System.out.println("merkle root: " + genesisBlockTestnet.getMerkleRoot().toString());
+        System.out.println("time: " + genesisBlockTestnet.getTime());
+
 
     }
 }
